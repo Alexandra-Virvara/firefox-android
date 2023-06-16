f6a8e39452305e62b3fb9f39f797518991ac157f
alexandra.virvara <avirvara@mozilla.com>
2023-06-16 19:40:47 +0300

    Bug 1837984: Adjust the spacing between the normal, private and sync tabs

--- fenix/app/src/main/java/org/mozilla/fenix/tabstray/TabsTrayBanner.kt
+++ fenix/app/src/main/java/org/mozilla/fenix/tabstray/TabsTrayBanner.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -56,6 +55,7 @@ import org.mozilla.fenix.compose.annotation.LightDarkPreview
 import org.mozilla.fenix.theme.FirefoxTheme
 
 private val ICON_SIZE = 24.dp
+private const val MAX_WIDTH_TAB_ROW = 0.5f
 
 /**
  * Top-level UI for displaying the banner in [TabsTray].
@@ -167,7 +167,7 @@ private fun SingleSelectBanner(
             CompositionLocalProvider(LocalRippleTheme provides DisabledRippleTheme) {
                 TabRow(
                     selectedTabIndex = selectedPage.ordinal,
-                    modifier = Modifier.width(180.dp),
+                    modifier = Modifier.fillMaxWidth(MAX_WIDTH_TAB_ROW),
                     backgroundColor = Color.Transparent,
                     contentColor = selectedColor,
                     divider = {},
@@ -177,6 +177,7 @@ private fun SingleSelectBanner(
                         onClick = { onTabPageIndicatorClicked(Page.NormalTabs) },
                         modifier = Modifier
                             .fillMaxHeight()
+                            .fillMaxWidth(1f)
                             .testTag(TabsTrayTestTag.normalTabsPageButton),
                         selectedContentColor = selectedColor,
                         unselectedContentColor = inactiveColor,
@@ -189,6 +190,7 @@ private fun SingleSelectBanner(
                         onClick = { onTabPageIndicatorClicked(Page.PrivateTabs) },
                         modifier = Modifier
                             .fillMaxHeight()
+                            .fillMaxWidth(1f)
                             .testTag(TabsTrayTestTag.privateTabsPageButton),
                         icon = {
                             Icon(
@@ -205,6 +207,7 @@ private fun SingleSelectBanner(
                         onClick = { onTabPageIndicatorClicked(Page.SyncedTabs) },
                         modifier = Modifier
                             .fillMaxHeight()
+                            .fillMaxWidth(1f)
                             .testTag(TabsTrayTestTag.syncedTabsPageButton),
                         icon = {
                             Icon(
